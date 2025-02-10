e0b341d9c70403bf410c78c824aa1223d6829ce6
SelmiNazeeb <selmisellu@gmail.com>
2025-02-10 17:40:13 +0530

    modified exceptionhandling.py

--- 007_Python/ExceptionHandling.py
+++ 007_Python/ExceptionHandling.py
@@ -2,7 +2,7 @@ try:
     #print(10+50)    #output - 60
     #print(10+'50')  #output - something went wrong
     #print(10/0)    #zeroDivision error
-    print(int("123d"))  #value error
+    print(int("5+z"))  #value error
 except TypeError:
     print("something went wrong")
 except ValueError:
